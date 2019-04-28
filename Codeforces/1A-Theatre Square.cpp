cf1f0dd655a4cf1d0b60395c5317fee28ecc8ff8
Azijur Rahman Sheatu <azijursheatu@gmail.com>
2019-04-29 01:03:34 +0600

    1A-Theatre Square
    Problem link: http://codeforces.com/problemset/problem/1/A

---
+++ Codeforces/1A-Theatre Square.cpp
@@ -0,0 +1,23 @@
+/*
+Problem link: http://codeforces.com/problemset/problem/1/A
+Here the code using a tricky formula : ((a+n-1)/n)*((b+n-1)/n); which may seems like a special formula but it is actually a very elegant way of ceiling the value; We just add the value of divider minus one with the divident then divide by the divider.
+
+That mean: x = (a+n-1)/n; work same as: 
+            1. x = a/n;if(a%n!=0) x++;
+            Or
+            2. x = celi(a/n); 
+            Or
+            3. x = a/n+(a%n==0)?0:1;
+*/
+
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+int main()
+{
+    ll a,b,n,ans;
+    cin>>a>>b>>n;
+    ans=((a+n-1)/n)*((b+n-1)/n);
+    cout<<ans<<endl;
+    return 0;
+}
